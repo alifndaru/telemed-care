f4dbe3b3f993704cf8f895be8b29b42bdee81950
arenga <arengapinnatamarzuki@gmail.com>
2024-11-11 22:48:56 +0700

    Add views structure folder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('test123'),
+            'remember_token' => Str::random(10),
         ]);
     }
 }
