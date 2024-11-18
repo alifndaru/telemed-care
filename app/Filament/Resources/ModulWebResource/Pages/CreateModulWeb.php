134a9c9c9a034009a3e517807ccede418ddc8328
alifndaru <alifndaru8@gmail.com>
2024-11-18 23:19:51 +0700

    chore: Update redirect URL in Filament resource pages

--- app/Filament/Resources/ModulWebResource/Pages/CreateModulWeb.php
+++ app/Filament/Resources/ModulWebResource/Pages/CreateModulWeb.php
@@ -9,4 +9,8 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateModulWeb extends CreateRecord
 {
     protected static string $resource = ModulWebResource::class;
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
