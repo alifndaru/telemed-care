15c972df680554976fc8a71525127f8d42125a9f
AndhikaPrasetya <andhikaprasetya542@gmail.com>
2024-11-22 16:56:39 +0700

    getdata-tenaga-layanan

---
+++ app/Http/Controllers/TenagaLayananController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class TenagaLayananController extends Controller
+{
+    public function index()
+    {
+
+        $data = User::whereNotNull('spesialis_id')->whereNotNull('klinik_id')
+            ->with([
+                'spesialis:id,name',
+                'klinik:id,name,province_id',
+                'klinik.provinsi:id,name'
+            ])
+            ->orderBy('name')
+            ->take(10)
+            ->get();
+        return view('tenaga-layanan', compact('data'));
+    }
+
+    public function getLayanan($category)
+    {
+        $data = User::where('spesialis_id', $category)
+            ->with([
+                'spesialis:id,name',
+                'klinik:id,name,province_id',
+                'klinik.provinsi:id,name'
+            ])
+            ->orderBy('name')
+            ->get();
+        return response()->json($data);
+    }
+}
--- resources/views/tenaga-layanan.blade.php
+++ resources/views/tenaga-layanan.blade.php
@@ -1,66 +1,84 @@
 @extends('layouts.app')
 
-@section('title', 'Home Page | PKBI CARE')
+@section('title', 'Tenaga Layanan | PKBI CARE')
 
 @section('content')
     <div class="provider-container mt-36 container ">
-        <div class="provider-list md:flex flex-row p-10 justify-between h-full  lg:divide-x-4 divide-sky-600">
+        <div class="provider-list md:flex flex-row p-10 justify-between h-full">
             <div class="provider-category w-auto">
                 <h3 class="text-xl font-bold text-sky-600">DATA LAYANAN KAMI</h3>
                 <div class="flex flex-col  p-5 gap-3">
-                    <a href="" class="font-semibold text-sky-600 text-md"><i class="fas fa-caret-right mr-2"></i>Kontrasepsi</a>
-                    <a href="" class="font-semibold text-sky-600 text-md"><i class="fas fa-caret-right mr-2"></i>Infeksi Menular Seksual & Infeksi Saluran Reproduksi</a>
-                    <a href="" class="font-semibold text-sky-600 text-md"><i class="fas fa-caret-right mr-2"></i>HIV / AIDS</a>
-                    <a href="" class="font-semibold text-sky-600 text-md"><i class="fas fa-caret-right mr-2"></i>Konseling KTD</a>
-                    <a href="" class="font-semibold text-sky-600 text-md"><i class="fas fa-caret-right mr-2"></i>Kesehatan Ibu & Anak </a>
-                    <a href="" class="font-semibold text-sky-600 text-md"><i class="fas fa-caret-right mr-2"></i>Papsmear</a>
+                    <a href="javascript:void(0)" class="category font-semibold text-sky-600 text-md" data-category="1"><i class="fas fa-caret-right mr-2"></i>Kontrasepsi</a>
+                    <a href="javascript:void(0)" class="category font-semibold text-sky-600 text-md" data-category="2"><i class="fas fa-caret-right mr-2"></i>Infeksi Menular Seksual & Infeksi Saluran Reproduksi</a>
+                    <a href="javascript:void(0)" class="category font-semibold text-sky-600 text-md" data-category="3"><i class="fas fa-caret-right mr-2"></i>HIV / AIDS</a>
+                    <a href="javascript:void(0)" class="category font-semibold text-sky-600 text-md" data-category="4"><i class="fas fa-caret-right mr-2"></i>Konseling KTD</a>
+                    <a href="javascript:void(0)" class="category font-semibold text-sky-600 text-md" data-category="5"><i class="fas fa-caret-right mr-2"></i>Kesehatan Ibu & Anak </a>
+                    <a href="javascript:void(0)" class="category font-semibold text-sky-600 text-md" data-category="6"><i class="fas fa-caret-right mr-2"></i>Papsmear</a>
                     <div>
-                        <a href="#" onclick="toggleDropdown('dropdownSGBV')" class="text-blue-600 hover:text-blue-800 font-semibold block"><i class="fas fa-caret-right mr-2"></i>SGBV</a>
+                        <a href="#" onclick="toggleDropdown('dropdownSGBV')" class="text-blue-600  font-semibold block"><i class="fas fa-caret-right mr-2"></i>SGBV</a>
                         <div id="dropdownSGBV" class="hidden pl-4 mt-2 space-y-2">
-                          <a href="#" class="text-blue-500 hover:text-blue-700 block"><i class="fas fa-caret-right mr-2"></i>Kekerasan Dalam Rumah Tangga</a>
-                          <a href="#" class="text-blue-500 hover:text-blue-700 block"><i class="fas fa-caret-right mr-2"></i>Kekerasan Dalam Berpacaran</a>
-                          <a href="#" class="text-blue-500 hover:text-blue-700 block"><i class="fas fa-caret-right mr-2"></i>Kekerasan Seksual</a>
+                          <a href="javascript:void(0)" class="category text-sky-600 hover:text-sky-700 block" data-category="1">Kekerasan Dalam Rumah Tangga</a>
+                          <a href="javascript:void(0)" class="category text-sky-600 hover:text-sky-700 block" data-category="2">Kekerasan Dalam Berpacaran</a>
+                          <a href="javascript:void(0)" class="category text-sky-600 hover:text-sky-700 block" data-category="3">Kekerasan Seksual</a>
                         </div>
                       </div>
 
-                    <a href="" class="font-semibold text-sky-600 text-lg"><i class="fas fa-caret-right mr-2"></i>Psikologi </a>
+                    <a href="javascript:void(0)" class="category font-semibold text-sky-600 text-lg " data-category="4"><i class="fas fa-caret-right mr-2"></i>Psikologi </a>
                 </div>
             </div>
             
+            
               
-            <div class="dokter-details  mt-12 lg:mt-0 divide-y-2 divide-sky-600   ">
-                <div class="dokter-desc hidden lg:block mt-10 lg:mt-0 lg:ml-10 mb-10">
-                    <p class="text-sky-600">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius ipsum tenetur
-                        expedita facere culpa;
-                        alias pariatur voluptas cupiditate ipsa saepe. Voluptates nam, laboriosam quo dolor earum id!
-                        Inventore, quidem quo.</p>
-                </div>
-                @foreach ($data as $item)
-                    
-                <div class="dokter-list lg:ml-10 ">
-                    <div class="flex flex-col lg:flex-row p-3 lg:items-center ">
-                        <div class="dokter-profile flex flex-row items-center">
-                            <img src="\images\user.png" class="w-20 h-20" alt="image:" />
-                            <div class="dokter-info ml-8 p-2">
-                                <div class="dokter-name font-bold">{{$item->name}}</div>
-                                <div class="dokter-title font-normal">SP.BGK</div>
-                                <div class="dokter-desc mt-4">
-                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa, dolor aut. Minus quod
-                                        placeat dolorum ut similique debitis officia eligendi </p>
+            <div class="dokter-details  w-10/12 mt-12 mx-auto lg:mt-0  ">
+                <div class="mb-4">
+                    <nav class="flex px-5 py-3 text-gray-700 border border-gray-200 rounded-lg bg-gray-50" aria-label="Breadcrumb">
+                        <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                            <li class="inline-flex items-center">
+                                <a href="#" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-sky-600 dark:text-gray-400">
+                                    Layanan
+                                </a>
+                            </li>
+                           
+                            <li aria-current="page">
+                                <div class="flex items-center">
+                                    <svg class="rtl:rotate-180  w-3 h-3 mx-1 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                                    </svg>
+                                    <span id="breadcrumb-category" class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400"></span>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="profile-action flex lg:flex-col justify-end lg:justify-start gap-1 ">
-                            <a href="#" class="  flex lg:flex-col gap-2 lg:gap-0 rounded-lg text-xs lg:text-sm py-1 px-2 lg:py-1 lg:px-2  bg-sky-600 lg:justify-center items-center  text-white text-center  font-bold "><span><img src="\images\user.png" alt="" class="  w-6 mx-auto "></span>PROFILE</a>
-                            <a href="#" class="flex lg:flex-col gap-2 lg:gap-0 rounded-lg text-xs lg:text-sm py-1 px-2 lg:py-1 lg:px-2  bg-yellow-400 lg:justify-center items-center text-sky-700 font-bold  "><span><img src="\images\chat.png" alt="" class=" w-6 mx-auto"></span>KONSULTASI </a>
-                        </div>
-                    </div>
+                            </li>
+                        </ol>
+                    </nav>
                 </div>
-               
-                @endforeach
-                
-                
-            </div>
+                <div id="result-container">
+
+                    @foreach ($data as $item)
+                        
+                    <div class="flex flex-col lg:flex-row justify-between p-3 lg:items-center shadow-lg rounded-lg mb-4 border">
+                        <div class="dokter-profile flex flex-row items-center">
+                            <img src="https://plus.unsplash.com/premium_photo-1658506671316-0b293df7c72b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="h-auto max-w-40" alt="image:" />
+                            <div class="dokter-info ml-8 p-2">
+                            <div class="dokter-name font-bold text-sky-600 text-lg">{{$item->name}}</div>
+                            <div class="dokter-title font-bold text-slate-300 text-sm">{{$item->spesialis->name}}</div>
+                        <div class="dokter-desc mt-4">
+                         <p class="flex gap-[13px] text-sky-600 items-center text-sm"><i class="fas fa-clinic-medical   "></i>{{$item->klinik->name}}</p>
+                         <p class="flex gap-4 text-sky-600 items-center text-sm"><i class="fas fa-map-marker-alt ml-[3px]  "></i>{{$item->klinik->provinsi->name}}</p>
+                        </div>
+                        </div>
+                        </div>
+                        <div class="profile-action flex lg:flex-col justify-end lg:justify-start gap-1">
+                        <a href="#" class="flex lg:flex-col gap-2 lg:gap-0 rounded-lg text-xs lg:text-sm py-1 px-2 lg:py-1 lg:px-2 bg-sky-600 lg:justify-center items-center text-white text-center font-bold">
+                            <span><i class="far fa-user"></i></span>PROFILE
+                        </a>
+                        <a href="#" class="flex lg:flex-col gap-2 lg:gap-0 rounded-lg text-xs lg:text-sm py-1 px-2 lg:py-1 lg:px-2 bg-yellow-400 lg:justify-center items-center text-sky-700 font-bold">
+                        <span><i class="far fa-comments"></i></span>KONSULTASI
+                        </a>
+                        </div>
+                     </div>
+                    @endforeach
+                </div>
+
+           </div>
             </section>
         </div>
         @endsection
@@ -73,7 +91,61 @@
            } else {
              dropdown.classList.add('hidden');
            }
-         }
+         };
+
+         $(document).ready(function(){
+            $('.category').on('click', function(){
+                let category = $(this).data('category');
+                let categoryName = this.textContent.trim();
+
+                let breadcrumbCategory = document.getElementById('breadcrumb-category');
+                breadcrumbCategory.textContent = categoryName;
+
+                document.querySelectorAll('.category i').forEach(icon=>{
+                                icon.classList.remove('text-black');
+                            });
+
+                const icon = this.querySelector('i');
+                icon.classList.add('text-black');
+
+                $.ajax({
+                    url: `/tenaga-layanan/${category}`,
+
+                    type: 'GET',
+                    success: function(data){
+                        let htmlContent = "";
+
+                        data.forEach(function(item){
+                            htmlContent += `
+                             <div class="flex flex-col lg:flex-row justify-between p-3 lg:items-center shadow-lg rounded-lg mb-4 border">
+                        <div class="dokter-profile flex flex-row items-center">
+                            <img src="https://plus.unsplash.com/premium_photo-1658506671316-0b293df7c72b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="h-auto max-w-40" alt="image:" />
+                            <div class="dokter-info ml-8 p-2">
+                            <div class="dokter-name font-bold text-sky-600 text-lg">${item.name}</div>
+                            <div class="dokter-title font-bold text-slate-300 text-sm">${item.spesialis.name}</div>
+                        <div class="dokter-desc mt-4">
+                         <p class="flex gap-[13px] text-sky-600 items-center text-sm"><i class="fas fa-clinic-medical   "></i>${item.klinik.name}</p>
+                         <p class="flex gap-4 text-sky-600 items-center text-sm"><i class="fas fa-map-marker-alt ml-[3px]  "></i>${item.klinik.provinsi.name}</p>
+                        </div>
+                        </div>
+                        </div>
+                        <div class="profile-action flex lg:flex-col justify-end lg:justify-start gap-1">
+                        <a href="#" class="flex lg:flex-col gap-2 lg:gap-0 rounded-lg text-xs lg:text-sm py-1 px-2 lg:py-1 lg:px-2 bg-sky-600 lg:justify-center items-center text-white text-center font-bold">
+                            <span><i class="far fa-user"></i></span>PROFILE
+                        </a>
+                        <a href="#" class="flex lg:flex-col gap-2 lg:gap-0 rounded-lg text-xs lg:text-sm py-1 px-2 lg:py-1 lg:px-2 bg-yellow-400 lg:justify-center items-center text-sky-700 font-bold">
+                        <span><i class="far fa-comments"></i></span>KONSULTASI
+                        </a>
+                        </div>
+                    </div>`;
+                        });
+                        $('#result-container').html(htmlContent);
+                    },
+                    error: function(error){console.error('error fetching data', error);
+                    }
+                });
+            })
+         })
          </script>
         @endsection
 
