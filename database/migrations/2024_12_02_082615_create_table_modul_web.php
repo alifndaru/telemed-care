2e763557d5b4722281cc60c9e37c98e153102908
alifndaru <alifndaru8@gmail.com>
2024-12-02 15:27:40 +0700

    chore: Rename migration file for modul_web table

--- database/migrations/2024_12_02_082615_create_table_modul_web.php
+++ database/migrations/2024_12_02_082615_create_table_modul_web.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('table_modul_web', function (Blueprint $table) {
+        Schema::create('modul_web', function (Blueprint $table) {
             $table->id();
             $table->string('namaWebsite');
             $table->string('Email');
