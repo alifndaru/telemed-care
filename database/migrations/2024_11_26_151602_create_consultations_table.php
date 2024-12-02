8ea4cc6c1a24f468e3924bb5684a622bb010b944
alifndaru <alifndaru8@gmail.com>
2024-12-02 14:24:22 +0700

    Refactor consultations table migration to use bigInteger for foreign key columns

--- database/migrations/2024_11_26_151602_create_consultations_table.php
+++ database/migrations/2024_11_26_151602_create_consultations_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('consultations', function (Blueprint $table) {
             $table->id();
-            $table->id('users_id');
-            $table->id('transactions_id');
+            $table->bigInteger('users_id');
+            $table->bigInteger('transactions_id');
             $table->string('judulKonsultasi');
             $table->text('penjelasan');
             $table->boolean('status')->default(true);
