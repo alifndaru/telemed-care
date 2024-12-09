1520c02f38dfb674fbca2d14cb0247a6b7dd53a6
AndhikaPrasetya <andhikaprasetya542@gmail.com>
2024-12-09 14:08:11 +0700

    tenaga layanan and tenaga provider

--- app/Http/Controllers/TenagaLayananController.php
+++ app/Http/Controllers/TenagaLayananController.php
@@ -9,28 +9,29 @@ use Illuminate\Support\Facades\Cache;
 
 class TenagaLayananController extends Controller
 {
+
     public function index()
     {
 
-        $data = Cache::remember('admin.layanan', 60, function () {
-            return Admin::whereNotNull('spesialis_id')->whereNotNull('klinik_id')
-                ->with([
-                    'spesialisasi:id,name',
-                    'klinik:id,namaKlinik,province_id',
-                    'klinik.provinsi:id,name'
-                ])
-                ->orderBy('name')
-                ->take(10)
-                ->get();
-        });
+       
+            $data = Admin::whereNotNull('pelayanan_id')->whereNotNull('klinik_id')->where('role_id', 3)
+            ->with([
+                'pelayanan:id,name',
+                'spesialis:id,name',
+                'klinik:id,namaKlinik,province_id',
+                'klinik.provinsi:id,name'
+            ])
+            ->orderBy('name')
+            ->get();
         return view('tenaga-layanan', compact('data'));
-    }
+    } 
 
     public function getLayanan($category)
     {
-        $data = Admin::where('spesialis_id', $category)
+        $data = Admin::where('pelayanan_id', $category)
             ->with([
-                'spesialisasi:id,name',
+                'pelayanan:id,name',
+                'spesialis:id,name',
                 'klinik:id,namaKlinik,province_id',
                 'klinik.provinsi:id,name'
             ])
@@ -38,4 +39,5 @@ class TenagaLayananController extends Controller
             ->get();
         return response()->json($data);
     }
+
 }
